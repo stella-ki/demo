0cc6df8c0108d0e7f6bb666fc26883b1756a25db
syki <neatouo@gmail.com>
2021-05-14 15:36:35 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello_world(){
+        return "hello, world2";
+
+    }
+
+    @GetMapping("/hello/{name}")
+    public String hello_name(@PathVariable String name, Model model){
+        model.addAttribute("name", name);
+        return "hello_name"; // Thymeleaf 템플릿 이름
+    }
+
+}
