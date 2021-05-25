ea56a6ef7891dc39068202dccef52a4422341414
syki <neatouo@gmail.com>
2021-05-25 14:18:57 +0900

    test test

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+class HelloControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+
+    @Autowired
+    HelloController mainController;
+
+    @BeforeEach
+    void beforeEach() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    void calculateMac() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/hello")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        )
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello, world2"));
+    }
+
+}
